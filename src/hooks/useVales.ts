2e6ad915522f69bc0e852b0859d278283d8e43d0
sofiasaless <sofiasaleswk@gmail.com>
2026-02-23 23:25:28 -0300

    fix: ordenação de vales a partir da data

--- src/hooks/useVales.ts
+++ src/hooks/useVales.ts
@@ -1,64 +1,87 @@
-import { useState } from "react"
-import { funcionarioFirestore } from "../firestore/funcionario.firestore"
-import { Vale } from "../schema/vale.shema"
-import { errorHookResponse, successHookResponse } from "../types/hookResponse.type"
+import { useState } from "react";
+import { funcionarioFirestore } from "../firestore/funcionario.firestore";
+import { Vale } from "../schema/vale.shema";
+import {
+  errorHookResponse,
+  successHookResponse,
+} from "../types/hookResponse.type";
+import { useQuery } from "@tanstack/react-query";
+import { converterTimestamp } from "../util/formatadores.util";
+
+export function useListarVales(idFuncionario: string) {
+  return useQuery({
+    queryKey: ["vales", idFuncionario],
+    queryFn: async () => {
+      const res = await funcionarioFirestore.encontrarPorId(idFuncionario);
+
+      const valesOrdenados = res.vales.sort((a, b) => {
+        const data1 = converterTimestamp(a.data_adicao)
+        const data2 = converterTimestamp(b.data_adicao)
+        
+        return data1.getTime() - data2.getTime();
+      });
+
+      return valesOrdenados;
+    },
+    refetchInterval: 30 * 60 * 1000,
+  });
+}
 
 export function useVales() {
-
-  const [isLoading, setIsLoading] = useState(false)
+  const [isLoading, setIsLoading] = useState(false);
 
   const adicionarVale = async (idFuncionario: string, vale: Vale) => {
-    setIsLoading(true)
+    setIsLoading(true);
     try {
-      await funcionarioFirestore.adicionarVale(idFuncionario, vale)
-      return successHookResponse()
+      await funcionarioFirestore.adicionarVale(idFuncionario, vale);
+      return successHookResponse();
     } catch (error: any) {
-      return errorHookResponse(error)
+      return errorHookResponse(error);
     } finally {
-      setIsLoading(false)
+      setIsLoading(false);
     }
-  }
+  };
 
   const adicionarVales = async (id: string, vales: Vale[]) => {
-    setIsLoading(true)
+    setIsLoading(true);
     try {
       vales.map(async (v) => {
-        await adicionarVale(id, v)
-      })
-      return successHookResponse()
+        await adicionarVale(id, v);
+      });
+      return successHookResponse();
     } catch (error: any) {
-      return errorHookResponse(error)
+      return errorHookResponse(error);
     } finally {
-      setIsLoading(false)
+      setIsLoading(false);
     }
-  }
+  };
 
   const removerVale = async (id: string, vale: Vale) => {
-    setIsLoading(true)
+    setIsLoading(true);
     try {
-      await funcionarioFirestore.removerVale(id, vale)
-      return true
+      await funcionarioFirestore.removerVale(id, vale);
+      return true;
     } catch (error: any) {
-      return false
+      return false;
     } finally {
-      setIsLoading(false)
+      setIsLoading(false);
     }
-  }
+  };
 
-  const [isLoadingVales, setIsLoadingVales] = useState(false)
-  const [vales, setVales] = useState<Vale[]>()
+  const [isLoadingVales, setIsLoadingVales] = useState(false);
+  const [vales, setVales] = useState<Vale[]>();
   const listarVales = async (id: string) => {
-    setIsLoadingVales(true)
+    setIsLoadingVales(true);
     try {
       const res = await funcionarioFirestore.encontrarPorId(id);
-      setVales(res.vales)
-      return res.vales
+      setVales(res.vales);
+      return res.vales;
     } catch (error) {
-      setVales(undefined)
+      setVales(undefined);
     } finally {
-      setIsLoadingVales(false)
+      setIsLoadingVales(false);
     }
-  }
+  };
 
   return {
     isLoading,
@@ -67,6 +90,6 @@ export function useVales() {
     removerVale,
     listarVales,
     vales,
-    isLoadingVales
-  }
+    isLoadingVales,
+  };
 }
