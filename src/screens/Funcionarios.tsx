799fa9afc7b6d736605a91789177b398124f9052
sofiasaless <sofiasaleswk@gmail.com>
2026-03-09 00:03:03 -0300

    fix: pagamento pendente

--- src/screens/Funcionarios.tsx
+++ src/screens/Funcionarios.tsx
@@ -1,21 +1,28 @@
-import { useFocusEffect } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import * as SplashScreen from 'expo-splash-screen';
-import { useCallback } from 'react';
+import {
+  NavigationProp,
+  useFocusEffect,
+  useNavigation,
+} from "@react-navigation/native";
+import { useFonts } from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
+import { useCallback, useEffect } from "react";
 import { Container } from "../components/Container";
 import { Header } from "../components/Header";
 import { ListaFuncionarios } from "../components/ListaFuncionarios";
+import { useListarMensalidades } from "../hooks/useMensalidades";
+import { useRestauranteConectado } from "../hooks/useRestaurante";
+import { Alert } from "react-native";
+import { RootStackParamList } from "../routes/StackRoutes";
 
 SplashScreen.preventAutoHideAsync();
 
 export const Funcionarios = () => {
-
   const [loaded, error] = useFonts({
-    'JetBrains-Regular': require('../../assets/fonts/JetBrainsMono-Regular.ttf'),
-    'JetBrains-Bold': require('../../assets/fonts/JetBrainsMono-Bold.ttf'),
-    'JetBrains-Medium': require('../../assets/fonts/JetBrainsMono-Medium.ttf'),
-    'JetBrains-SemiBold': require('../../assets/fonts/JetBrainsMono-SemiBold.ttf'),
-    'JetBrains-Italic': require('../../assets/fonts/JetBrainsMono-Italic.ttf')
+    "JetBrains-Regular": require("../../assets/fonts/JetBrainsMono-Regular.ttf"),
+    "JetBrains-Bold": require("../../assets/fonts/JetBrainsMono-Bold.ttf"),
+    "JetBrains-Medium": require("../../assets/fonts/JetBrainsMono-Medium.ttf"),
+    "JetBrains-SemiBold": require("../../assets/fonts/JetBrainsMono-SemiBold.ttf"),
+    "JetBrains-Italic": require("../../assets/fonts/JetBrainsMono-Italic.ttf"),
   });
 
   useFocusEffect(
@@ -23,7 +30,7 @@ export const Funcionarios = () => {
       if (loaded || error) {
         SplashScreen.hideAsync();
       }
-    }, [loaded, error])
+    }, [loaded, error]),
   );
 
   if (!loaded && !error) {
@@ -35,5 +42,5 @@ export const Funcionarios = () => {
       <Header title="Funcionários" />
       <ListaFuncionarios />
     </Container>
-  )
-}
+  );
+};
